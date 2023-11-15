8b5a7ca6479332c4bc67b727285a1ed7bafc6c84
Luciano Ristori <Luciano@fnal.gov>
2023-11-15 12:05:18 -0600

    add macro plotEff to plot efficiency as a function of phi, eta, 1/pt

---
+++ HT_toyPrototype/my_include.h
@@ -0,0 +1,16 @@
+
+
+#include <iosfwd>
+#include <iostream>
+#include <cstddef>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include "TH1.h"
+#include "TH2.h"
+#include "TProfile.h"
+#include "TGraph.h"
+#include "TGraphAsymmErrors.h"
+#include "TFile.h"
+#include "TCanvas.h"
+#include "TLegend.h"
---
+++ HT_toyPrototype/plotEff.C
@@ -0,0 +1,139 @@
+
+// to run under root:
+
+//  root> .x plotEff.C
+
+
+#include "my_include.h"
+
+
+//_________________________________________________
+//
+// function for binomial error
+//
+
+void BinomialError(Double_t n,Double_t N,Double_t* p,Double_t* err_low,Double_t* err_high){
+
+  *p = n/N;
+  *err_high = (sqrt(n + 0.25 - n*n/N) + 0.5 - n/N)/(N + 1.);
+  *err_low  = (sqrt(n + 0.25 - n*n/N) - 0.5 + n/N)/(N + 1.);
+  return;
+}
+
+
+/******************************************
+Divides two histograms and makes graph with asymmetric
+binomial Punzi errors.
+Histograms are taken from root files
+******************************************/
+
+
+TGraph* makeEffGraphFromFile(const char * file_num,const char * hist_num,
+                             const char * file_den, const char * hist_den) {
+
+
+  // kludge: allocate mem for 1000 bins maximum
+  // --- correct size should be dynamically allocated ---
+
+  Double_t eff[1000];
+  Double_t erry_high[1000];
+  Double_t erry_low[1000];
+  Double_t errx_high[1000];
+  Double_t errx_low[1000];
+  Double_t x_value[1000];
+
+  // clone denominator histogram
+
+  TFile *f = new TFile(file_den);
+  
+  TH1F* hist = (TH1F*)f->Get(hist_den);
+  TH1F* den_hist = (TH1F*)hist->Clone();
+
+  // clone numerator histogram
+
+  f = new TFile(file_num);
+
+  hist = (TH1F*)f->Get(hist_num); 
+  TH1F* num_hist=(TH1F*)hist->Clone();
+
+  // find number of bins to plot
+
+  Int_t nbins = num_hist->GetNbinsX();
+  if(den_hist->GetNbinsX() < nbins) nbins = den_hist->GetNbinsX();// minimum nbins
+  if(nbins > 1000) nbins=1000;// max 1000 bins 
+
+  // fill arrays with eff and errors
+
+   for(Int_t ibin=0;ibin<nbins;++ibin){
+     
+     Double_t x1 = num_hist->GetBinContent(ibin+1);// skip undf & ovf
+     Double_t x2 = den_hist->GetBinContent(ibin+1);
+
+     x_value[ibin]= num_hist->GetBinCenter(ibin+1);
+     errx_high[ibin]=num_hist->GetBinWidth(ibin+1)/2.;
+     errx_low[ibin]=num_hist->GetBinWidth(ibin+1)/2.;
+
+     if(x2 != 0.){
+       // estimate efficiency and asymmetric binomial errors
+       BinomialError(x1,x2,&eff[ibin],&erry_low[ibin],&erry_high[ibin]);
+     }
+     else{
+       // set eff to 1/2 +/- 1/2
+      eff[ibin]=0.5;
+      erry_high[ibin]=0.5;
+      erry_low[ibin]=0.5;
+     }
+   }
+
+ // create graph of efficiency
+
+   TGraph* gr  = new TGraphAsymmErrors(nbins,x_value,eff,errx_low,errx_high,erry_low,erry_high);
+
+   return gr;
+}
+
+
+//---------------------------------------------------------------
+
+void plotEff(){
+ 
+  TGraph * g_eff = makeEffGraphFromFile("AAAEventGeneration.root","HTrackPhiEff","AAAEventGeneration.root","HTrackPhi");
+
+ // first canvas position and size
+
+  Int_t x1 = 200;
+  Int_t y1 = 300;
+  Int_t width = 500;
+  Int_t height = 400;
+
+
+  	TCanvas* c1 = new TCanvas("c1","c1",x1,y1,width,height);
+	TGraph * g_eff1 = makeEffGraphFromFile("AAAEventGeneration.root","HTrackPhiEff","AAAEventGeneration.root","HTrackPhi");
+
+  	c1->SetGrid();
+  	c1->SetTitle("Phi");	
+  	g_eff1->SetTitle("efficiency vs phi");
+  	g_eff1->Draw("AP");
+  	
+  	TCanvas* c2 = new TCanvas("c2","c2",x1 + width+10 ,y1,width,height);
+	TGraph * g_eff2 = makeEffGraphFromFile("AAAEventGeneration.root","HTrackEtaEff","AAAEventGeneration.root","HTrackEta");
+
+  	c2->SetGrid();
+  	c2->SetTitle("Eta");
+  	g_eff2->SetTitle("efficiency vs eta");
+  	g_eff2->Draw("AP");
+  	
+  	TCanvas* c3 = new TCanvas("c3","c3",x1 + 2*width+20,y1,width,height); 	 
+	TGraph * g_eff3 = makeEffGraphFromFile("AAAEventGeneration.root","HTrackInvPtEff","AAAEventGeneration.root","HTrackInvPt");
+
+  	c3->SetGrid();
+  	c3->SetTitle("InvPt");
+  	g_eff3->SetTitle("efficiency vs invPt");
+  	g_eff3->Draw("AP");
+  	
+  	
+  	
+
+  return;
+}
+  
